75ceb42df89534f92b2a1e411896b2afa0a3ab93
Виталий Петрович <e2n0z1o5@gmail.com>
2018-11-06 07:06:03 +0300

    Refactored

--- NET1.S.2018.Petrovich.06.Test/SortJuggedArrayTest.cs
+++ NET1.S.2018.Petrovich.06.Test/SortJuggedArrayTest.cs
@@ -13,6 +13,167 @@ namespace NET1.S._2018.Petrovich._06.Test
     [TestFixture]
     public class SortJuggedArrayTest
     {
+        /// <summary>
+        /// Class containing comparison logic by sum.
+        /// </summary>
+        public class SortRowsBySum : IComparer
+        {
+            public int Compare(int[] firstSubArray, int[] secondSubArray)
+            {
+                if (ReferenceEquals(firstSubArray, secondSubArray))
+                    return 0;
+
+                if (ReferenceEquals(firstSubArray, null))
+                    return -1;
+
+                if (ReferenceEquals(secondSubArray, null))
+                    return 1;
+
+                return GetSum(firstSubArray) - GetSum(secondSubArray);
+            }
+        }
+
+        /// <summary>
+        /// Class containing comparison logic by sum.
+        /// </summary>
+        public class SortRowsBySumDescending : SortRowsBySum, IComparer
+        {
+            public new int Compare(int[] firstSubArray, int[] secondSubArray)
+                => -base.Compare(firstSubArray, secondSubArray);
+        }
+
+        /// <summary>
+        /// Class containing comparison logic by max element.
+        /// </summary>
+        public class SortRowsByMaxElement : IComparer
+        {
+            public int Compare(int[] firstSubArray, int[] secondSubArray)
+            {
+                if (ReferenceEquals(firstSubArray, secondSubArray))
+                    return 0;
+
+                if (firstSubArray == null)
+                    return -1;
+
+                if (secondSubArray == null)
+                    return 1;
+
+                return GetMax(firstSubArray) - GetMax(secondSubArray);
+            }
+        }
+
+        /// <summary>
+        /// Class containing comparison logic by max element.
+        /// </summary>
+        public class SortRowsByMaxElementDescending : SortRowsByMaxElement, IComparer
+        {
+            public new int Compare(int[] firstSubArray, int[] secondSubArray)
+                => -base.Compare(firstSubArray, secondSubArray);
+        }
+
+        /// <summary>
+        /// Class containing comparison logic by min element.
+        /// </summary>
+        public class SortRowsByMinElement : IComparer
+        {
+            public int Compare(int[] firstSubArray, int[] secondSubArray)
+            {
+                if (ReferenceEquals(firstSubArray, secondSubArray))
+                    return 0;
+
+                if (firstSubArray == null)
+                    return -1;
+
+                if (secondSubArray == null)
+                    return 1;
+
+                return GetMin(firstSubArray) - GetMin(secondSubArray);
+            }
+        }
+
+        /// <summary>
+        /// Class containing comparison logic by min element.
+        /// </summary>
+        public class SortRowsByMinElementDescending : SortRowsByMinElement, IComparer
+        {
+            public new int Compare(int[] firstSubArray, int[] secondSubArray)
+                => -base.Compare(firstSubArray, secondSubArray);
+        }
+
+        /// <summary>
+        /// Calculate sum of element in array.
+        /// </summary>
+        /// <param name="array">
+        /// Array.
+        /// </param>
+        /// <returns>
+        /// Sum.</returns>
+        public static int GetSum(int[] array)
+        {
+            if (ReferenceEquals(array, null))
+                return int.MinValue;
+
+            int sum = 0;
+            foreach (var item in array)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+
+        /// <summary>
+        /// Return max element of array.
+        /// </summary>
+        /// <param name="array">
+        /// Array.
+        /// </param>
+        /// <returns>
+        /// Max element.
+        /// </returns>
+        public static int GetMax(int[] array)
+        {
+            if (ReferenceEquals(array, null))
+                return int.MinValue;
+
+            int indexOfMax = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > array[indexOfMax])
+                {
+                    indexOfMax = i;
+                }
+            }
+
+            return array[indexOfMax];
+        }
+
+        /// <summary>
+        /// Return min element of array.
+        /// </summary>
+        /// <param name="array">
+        /// Array.
+        /// </param>
+        /// <returns>
+        /// Min element.
+        /// </returns>
+        public static int GetMin(int[] array)
+        {
+            if (ReferenceEquals(array, null))
+                return int.MinValue;
+
+            int indexOfMin = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < array[indexOfMin])
+                {
+                    indexOfMin = i;
+                }
+            }
+
+            return array[indexOfMin];
+        }
+
         [Test]
         public void BubbleSortJuggedArrayBySumRows1_RightIn_RightOut()
         {
@@ -52,7 +213,7 @@ namespace NET1.S._2018.Petrovich._06.Test
                 new[] { 1, 1, 1, 1 }
             };
 
-            BubbleSortJuggedArray(inJuggedArray, (x, y) => y.GetSum().CompareTo(x.GetSum()));
+            BubbleSortJuggedArray(inJuggedArray, (x, y) => GetSum(y).CompareTo(GetSum(x)));
             
             Assert.AreEqual(inJuggedArray, expectedJuggedArray);
         }
@@ -204,7 +365,7 @@ namespace NET1.S._2018.Petrovich._06.Test
                 new[] { 5, 14, 2, 0 }
             };
 
-            BubbleSortJuggedArray(inJuggedArray, 0, 1, (x, y) => x.GetMin().CompareTo(y.GetMin()));
+            BubbleSortJuggedArray(inJuggedArray, 0, 1, (x, y) => GetMin(x).CompareTo(GetMin(y)));
 
             Assert.AreEqual(inJuggedArray, expectedJuggedArray);
         }
@@ -268,7 +429,7 @@ namespace NET1.S._2018.Petrovich._06.Test
                 new[] { 10, 3 }
             };
 
-            SortJuggedArrayImplementationForTests.BubbleSortJuggedArray(inJuggedArray, (x, y) => x.GetMax().CompareTo(y.GetMax()));
+            SortJuggedArrayImplementationForTests.BubbleSortJuggedArray(inJuggedArray, (x, y) => GetMax(x).CompareTo(GetMax(y)));
 
             Assert.AreEqual(inJuggedArray, expectedJuggedArray);
         }
